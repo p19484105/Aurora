e4aaa2e5c4671a993e8729f170ff0d0b18ccda4d
Paxton Rivera <o3.aurora@icloud.com>
2025-05-09 18:54:11 -0400

    Initial Commit

---
+++ Aurora/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Aurora
+//
+//  Created by Paxton Rivera on 5/9/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
